671163e379284a22a1a32fac18957be5147cca1b
Takumi Endo <tq-ed@v7.com>
2016-06-02 08:49:02 +0900

    ライブラリバージョンアップ

--- src/main/java/example/SpringPlaygroundApplication.java
+++ src/main/java/example/SpringPlaygroundApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringPlaygroundApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringPlaygroundApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPlaygroundApplication.class, args);
+    }
 }
